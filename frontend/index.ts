e1b6ac7a23a2823cf21619dcc1c2eec71f0a8e5f
Marisa <marisa@clardy.eu>
2024-03-17 23:19:10 +0000

    More gracefully close sockets when changing pages

--- frontend/index.ts
+++ frontend/index.ts
@@ -4,8 +4,40 @@ import htmx from 'htmx.org';
 declare global {
   interface Window { htmx: typeof htmx }
 }
-window.htmx = htmx
+window.htmx = htmx;
 
 // Load HTMX Extensions
 require('htmx.org/dist/ext/ws');
 
+let ws: WebSocket|null = null;
+
+interface WSOpenEvent extends Event {
+  detail: {
+    elt: HTMLElement;
+    event: {
+      currentTarget: WebSocket;
+      target: WebSocket;
+      srcElement: WebSocket;
+    };
+    socketWrapper: any;
+  };
+}
+htmx.on('htmx:wsOpen', (evt: Event) => {
+  const e = evt as unknown as WSOpenEvent;
+  ws = e.detail.event.target;
+});
+
+htmx.on('htmx:wsClose', (evt: Event) => {
+  console.log(evt);
+});
+
+htmx.on('htmx:beforeSwap', (evt: Event) => {
+  if (ws) {
+    ws.send(JSON.stringify({
+      message: "",
+      close: true,
+    }));
+    ws.close()
+    ws = null
+  }
+})
