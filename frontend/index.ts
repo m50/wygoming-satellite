893957c5c9636e56b05ff9da1e73116d7efae68b
Marisa <marisa@clardy.eu>
2024-03-19 13:15:19 +0000

    Cleanup UI a bit

--- frontend/index.ts
+++ frontend/index.ts
@@ -48,3 +48,27 @@ htmx.on('htmx:beforeSwap', (evt: Event) => {
   }
   // console.log(evt)
 })
+
+htmx.onLoad(() => {
+  const sidebar = document.getElementById('sidebar');
+  const burgerMenu = document.getElementById('sidebar-burger');
+  const options = document.getElementById('sidebar-options');
+  const content = document.getElementById('content');
+  burgerMenu?.addEventListener('click', (evt) => {
+    // console.log(evt);
+    evt.stopImmediatePropagation()
+    const isVisible = options?.classList.contains('flex');
+    console.log(isVisible);
+    if (isVisible) {
+      options?.classList.replace('flex', 'hidden');
+      sidebar?.classList.remove('col-span-3');
+      content?.classList.remove('hidden');
+      content?.classList.add('col-span-3');
+    } else {
+      options?.classList.replace('hidden', 'flex');
+      sidebar?.classList.add('col-span-3');
+      content?.classList.add('hidden');
+      content?.classList.remove('col-span-3');
+    }
+  })
+});
